feebd1bfa3ceff0f5fa1c619dc8a4f1a4d03a3f1
Glen Smith <gsmith@spartaglobal.com>
2023-03-20 20:50:38 +0000

    Methods Lab exercises minus Random as incomplete

---
+++ Methods Lab/Labs/Methods_Lab_Starter/Methods_Lib/Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Methods_Lib
+{
+    public class Methods
+    {
+        // implement this method so it returns a tuple (weeks, days) 
+        // corresponding to a given number of days
+        public static (int weeks, int days) DaysAndWeeks(int totalDays)
+        {
+            if (totalDays < 0)
+            {
+                throw new ArgumentOutOfRangeException("totalDays must not be negative");
+            }
+            else
+            {
+                var nWeeks = totalDays / 7;
+                var nDays = totalDays % 7;
+                return (nWeeks, nDays);
+            }
+
+        }
+
+        public static int RollDice(Random rng)
+        {
+            var num1 = rng.Next(1, 7);
+            var num2 = rng.Next(1, 7);
+            return num1 + num2;
+        }
+
+        public static (int squared,int cubed ,double squareRoot) PowersRoot (int inputNumber)
+        {
+            var snumber = inputNumber * inputNumber;
+            var cnumber = inputNumber * inputNumber * inputNumber;
+            var srnumber = Math.Sqrt(inputNumber);
+
+            return (snumber, cnumber, Math.Round(srnumber, 3));
+        }
+
+    }
+
+
+}
---
+++ Methods Lab/Labs/Methods_Lab_Starter/Methods_Tests/DiceTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using Methods_Lib;
+
+namespace Methods_Tests
+{
+    public class DiceTests
+    {
+
+        //POWERS
+        [TestCase(10, 100, 1000,3.162)]
+
+        public void GivenANumber_Squared_Cubed_And_SquareRoot_Calculations_ReturnsCorrectTuple
+            
+            (int inputNumber, int expectedSquaredNumber, int expectedCubedNumber, double expectedSquareRootNumber)
+        {
+            var answer = Methods.PowersRoot(inputNumber);
+            Assert.That(answer.squared, Is.EqualTo(expectedSquaredNumber), "Squared");
+            Assert.That(answer.cubed, Is.EqualTo(expectedCubedNumber), "Cubed");
+            Assert.That(answer.squareRoot, Is.EqualTo(expectedSquareRootNumber), "Square Root");
+        }
+
+        //Roll Dice
+
+        //[TestCase(12)]
+
+        //public void GivenANumber_SumOfRollDiceReturnsSumLessThanThirteen (Random rng)
+        //{
+        //    var maximumNumber = 13;
+        //    var answer = Methods.RollDice(rng);
+        //    Assert.That(answer.rng, Is.EqualTo(maximumNumber));
+        //}
+
+
+    }
+}
