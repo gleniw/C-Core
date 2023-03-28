123a5da15693a02eb4591fad62e18070d1bb111d
Glen Smith <gsmith@spartaglobal.com>
2023-03-28 21:38:30 +0100

    Added Collection Lab Starter - Still some tests to pass

---
+++ Collections_Lab_Starter/Collections_Lab/Collections_Lib/CollectionsExercises.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections_Lib
+{
+    public class CollectionsExercises
+    {
+
+        /* removes and returns the next num entries in the queue, as a comma separated string */
+        public static string NextInQueue(int num, Queue<string> queue)
+        {
+            var nextInQueue = new Queue<string>();
+
+            //if (queue.Count == num)
+            //{
+
+            //}
+
+            if (queue.Count > 0)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    nextInQueue.Enqueue(queue.Dequeue());
+                }
+            }
+
+            return $"{nextInQueue}";
+        }
+
+        /* uses a Stack to create and return array of ints in reverse order to the one supplied */
+        public static int[] Reverse(int[] original)
+        {
+            int [] reversed = new int[original.Length];
+
+            var newStack = new Stack<int>();
+            foreach (int i in original)
+            {
+                newStack.Push(i);
+            }
+            for (int i = 0; i < original.Length; i++)
+            {
+                reversed[i] = newStack.Pop();
+            }
+            return reversed;
+
+        }
+        // using a Dictionary, counts and returns (as a string) the occurence of the digits 0-9 in the given string
+        public static string CountDigits(string input)
+        {
+            input = input.Trim();
+
+            var countDict = new Dictionary<char, int>();
+
+            string dictionaryString = "";
+
+            foreach (var c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    if (countDict.ContainsKey(c))
+                    {
+                        countDict[c]++;
+
+                    }
+                    else
+                    {
+                        countDict.Add(c, 1);
+                    }
+
+                    foreach (KeyValuePair<char, int> keyValues in countDict)
+                    {
+                        dictionaryString += "[" + keyValues.Key + " ," + keyValues.Value + "]";
+                    }
+
+                }
+
+
+            }
+
+            return dictionaryString;
+        }
+
+    }
+}
---
+++ Collections_Lab_Starter/Collections_Lab/Collections_Lib/ListExercises.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Lib
+{
+    public class ListExercises
+    {
+        // returns a list of all the integers between 1 to max inclusive
+        // that are multiples of 5
+        public static List<int> MakeFiveList(int max)
+        {
+            var makeFiveList = new List<int>();
+
+            for (int i = 1; i <= max; i++)
+            {
+                if (i % 5 == 0)
+                    makeFiveList.Add(i);
+            }
+            return makeFiveList;
+        }
+
+        // returns a list of all the strings in sourceList that start with the letter 'A' or 'a'
+        public static List<string> MakeAList(List<string> sourceList)
+        {
+            var makeAList = new List<String>();
+
+            foreach (string sourceString in sourceList)
+            {
+                if (sourceString.StartsWith("A") || sourceString.StartsWith("a"))
+                makeAList.Add(sourceString);
+            }
+            return makeAList;
+        }
+    }
+}
