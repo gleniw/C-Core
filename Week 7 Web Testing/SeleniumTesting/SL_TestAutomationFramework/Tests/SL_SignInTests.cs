7f7111dd5048bb67dd13baa5a138ef20681f3986
Glen Smith <gsmith@spartaglobal.com>
2023-04-04 12:06:07 +0100

    Framework complete and first test added

---
+++ Week 7 Web Testing/SeleniumTesting/SL_TestAutomationFramework/Tests/SL_SignInTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using SL_TestAutomationFramework.lib.pages;
+
+namespace SL_TestAutomationFramework.Tests
+{
+    public class SL_SignInTests
+    {
+        private SL_Website<ChromeDriver> SL_Website = new();
+        
+        [Test]
+        [Category ("Happy Path")]
+
+        public void GivenIAmOnTheHomePage_WhenIEnterAValidEmailAndPassword_IshouldLandOnTheInventoryPage()
+        {
+            SL_Website.SeleniumDriver.Manage().Window.Maximize();
+
+            SL_Website.SL_Homepage.VisitHomePage();
+            
+            SL_Website.SL_Homepage.EnterUserName(AppConfigReader.UserName);
+            
+            SL_Website.SL_Homepage.EnterPassword(AppConfigReader.Password);
+
+            SL_Website.SL_Homepage.ClickLogInButton();
+
+            Assert.That(SL_Website.SeleniumDriver.Url, Is.EqualTo(AppConfigReader.InventoryPageUrl));
+
+        }
+        //Performed after all methods 
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            SL_Website.SeleniumDriver.Quit(); //Quits all windows - also calls dispose
+            //SL_Website.SeleniumDriver.Dispose(); //Gets rid of unmanged resources 
+
+        }
+    }
+}
--- Week 7 Web Testing/SeleniumTesting/SL_TestAutomationFramework/lib/pages/SL_Homepage.cs
+++ Week 7 Web Testing/SeleniumTesting/SL_TestAutomationFramework/lib/pages/SL_Homepage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace SL_TestAutomationFramework.lib.pages
 {
-    internal class SL_Homepage
+    public class SL_HomePage
     {
+        private IWebDriver _seleniumDriver;
+
+        private string _homePageUrl = AppConfigReader.BaseUrl;
+        //Below is the best way to write out a property
+        private IWebElement _passwordField => _seleniumDriver.FindElement(By.Id("password")); // LAMBDA Invokes the method, cannot just assign
+
+        //Below is the full version
+        //private IWebElement _passwordField
+        //{
+        //    get { return _seleniumDriver.FindElement(By.Id("password")); }
+        //}
+
+        private IWebElement _usernameField => _seleniumDriver.FindElement(By.Id("user-name"));
+        private IWebElement _loginButton => _seleniumDriver.FindElement(By.Id("login-button"));
+
+        private IWebElement _alert => _seleniumDriver.FindElement(By.CssSelector("[data-test=\"error\"]"));
+
+        public SL_HomePage(IWebDriver seleniumDriver)
+        {
+            _seleniumDriver = seleniumDriver;
+        }
+
+        public void VisitHomePage() => _seleniumDriver.Navigate().GoToUrl(_homePageUrl);
+        public void EnterUserName(string username) => _usernameField.SendKeys(username);
+        public void EnterPassword(string password) => _passwordField.SendKeys(password);
+        public void ClickLogInButton() => _loginButton.Click();
+        public string CheckErrorMessage() => _alert.Text;
+
+        //Should have unit tests for the framework
     }
 }
