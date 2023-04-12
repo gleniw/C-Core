47d9ebc0d05aa860f2bf71a379414dd6d72eb852
Glen Smith <gsmith@spartaglobal.com>
2023-04-12 12:23:46 +0100

    API Testing

---
+++ Week 8 API Testing/APIClient/APIClientApp/Program.cs
@@ -0,0 +1,106 @@
+﻿using RestSharp;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+
+namespace APIClientApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Creating Client is the 1st step
+
+            //Encapsulates info we need to make the API call
+            //Allows us to send authenticated HTTPS requests
+
+            var restClient = new RestClient("https://api.postcodes.io/"); //Set base Url
+
+            //Set up request to API
+            var restRequest = new RestRequest();
+
+            //Optional as default is always Get
+            restRequest.Method = Method.Get;
+
+            //Extra information/instruction to be sent - Formatting
+            restRequest.AddHeader("Content-Type", "application/json"); //want to be returned as a JSON file
+
+            //Add the resource
+            string postcode = "EC2Y 5AS"; //Resource to query i.e. endpoint
+
+            restRequest.Resource = $"postcodes/{postcode.ToLower()}";
+
+            //Execute Responce - stored in singlePostcodeResponce variable
+            RestResponse singlePostcodeResponse = restClient.Execute(restRequest);
+
+            //Returns body as an unformatted string
+            Console.WriteLine("Responce content (string)");
+            Console.WriteLine(singlePostcodeResponse.Content);
+
+            Console.WriteLine("Responce Status (int)");
+            Console.WriteLine(singlePostcodeResponse.StatusCode); //Emun repsponce
+            Console.WriteLine((int)singlePostcodeResponse.StatusCode); //Cast to int for 200 status code
+
+
+            Console.WriteLine("Printing Headers");
+            foreach (var header in singlePostcodeResponse.Headers)
+            {
+                Console.WriteLine(header);
+            }
+
+            Console.WriteLine("\nFiltering with Key");
+            //Headers where the key is the date and if there are several values, return the first or default (zero/null)
+            var headers = singlePostcodeResponse.Headers;
+            var responseDateHeader = headers.Where(h => h.Name == "Date")
+                .Select(h => h.Value).FirstOrDefault(); //First or Default will return first that matches
+            Console.WriteLine(responseDateHeader);
+
+
+            Console.WriteLine("Copied from Postman - API Request in C# Code");
+
+            var options = new RestClientOptions("https://api.postcodes.io")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options); 
+            var bulkPostcodeRequest = new RestRequest("/postcodes/", Method.Post);
+            bulkPostcodeRequest.AddHeader("Content-Type", "application/json");
+
+            //var body = @"{" + "\n" + @" ""postcodes"" : [""OX49 5NU"", ""M32 0JG"", ""NE30 1DP""]" + "\n" + @"}"; //From Postman
+            var postcodes = new
+            {
+                Postcodes = new string[] { "OX49 5NU", "M32 0JG", "NE30 1DP"} //Postcodes is an anon type with a bunch of String arrays - Read Only
+            };
+            //Nish code to replce Postman
+
+            //request.AddStringBody(body, DataFormat.Json); //Treat String like a JSON String //From Postman
+            //request.AddJsonBody(postcodes);
+            bulkPostcodeRequest.AddJsonBody(postcodes);
+            RestResponse bulkPostcodeResponse = await client.ExecuteAsync(bulkPostcodeRequest); //Quick Actions - Make method Async
+            Console.WriteLine(bulkPostcodeResponse.Content);
+
+            //Serialise to Json String - taking Postcodes anon object and converting to Json String
+
+            //NewtonSoft to Serilaise and Query
+
+            Console.WriteLine("Formatted JObject");
+            var singlePostJsonResponse = JObject.Parse(singlePostcodeResponse.Content);
+            Console.WriteLine("\n Response content as a Jobject");
+            Console.WriteLine(singlePostJsonResponse);
+
+            Console.WriteLine("Return Status");
+            Console.WriteLine(singlePostJsonResponse["status"]); //Query JObject - Specify required Key
+
+            Console.WriteLine("Return Admin District");
+            Console.WriteLine(singlePostJsonResponse["result"]["admin_district"]);
+
+            Console.WriteLine("Json Array of Objects");
+            //Returning Specific item from Json object list
+            var bulkPostcodeJsonResponce = JObject.Parse(bulkPostcodeResponse.Content);
+            var adminDistrict = bulkPostcodeJsonResponce["result"][1]["result"]["admin_district"];
+            Console.WriteLine($"\nAdmin District of 2nd Post code {adminDistrict}");
+
+
+
+        }
+    }
+}
