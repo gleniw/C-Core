5bedbd04c7de7eef3f1f1b83a141459f1939dfbe
Glen Smith <gsmith@spartaglobal.com>
2023-04-12 16:36:06 +0100

    Additional Test added - now passing

---
+++ Week 8 API Testing/APIClient/APITestApp/SinglePostcodeService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace APITestApp
+{
+    public class SinglePostcodeService
+    {
+        #region Properties
+        //Handles comms with API
+        public RestClient Client { get; set; }
+
+        public RestResponse Response { get; set; }
+
+        public JObject ResponseContent { get; set; }
+
+        public SinglePostcodeResponse ResponseObject { get; set; }
+
+        #endregion
+
+        public SinglePostcodeService()
+        {
+            Client = new RestClient(AppConfigReader.BaseUrl);
+        }
+
+        public async Task MakeRequestAsync(string postcode)
+        {
+            var request = new RestRequest();
+            request.AddHeader("Content-Type", "application/json");
+            request.Resource = $"postcodes/{postcode}";
+            Response = await Client.ExecuteAsync(request);
+            ResponseContent = JObject.Parse(Response.Content);
+            ResponseObject = JsonConvert.DeserializeObject<SinglePostcodeResponse>(Response.Content);
+        }
+
+        //Copy to output, copy if newer
+    }
+}
---
+++ Week 8 API Testing/APIClient/APITestApp/SinglePostcodeServiceTests/WhenTheSinglePostcodeServiceIsCalled_WithValidPostcode.cs
@@ -0,0 +1,76 @@
+﻿
+namespace APITestApp.SinglePostcodeServiceTests
+{
+    [Category("Happy")]
+    public class WhenTheSinglePostcodeServiceIsCalled_WithValidPostcode
+    {
+        private SinglePostcodeService _singlePostcodeService;
+
+        [OneTimeSetUp] 
+
+        public async Task OneTimeSetupAsync() //This method needs to be Async if called another Async method
+        {
+            _singlePostcodeService = new SinglePostcodeService();
+            await _singlePostcodeService.MakeRequestAsync("EC2Y 5AS");
+            //await _singlePostcodeService.MakeRequestAsync("EC2Y 5AS").GetAwaiter().GetResult; //Alternative to changing to Async method
+        }
+
+        [Test]
+        public void StatusIs200_InJsonResponseBody()
+        {
+            Assert.That(_singlePostcodeService.ResponseContent["status"].ToString(), Is.EqualTo("200"));
+        }
+
+        [Test]
+
+        public void StatusIs200_InResponseHeader()
+        {
+            Assert.That((int)_singlePostcodeService.Response.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void CorrectPostcodeIsReturned()
+        {
+            var result = _singlePostcodeService.ResponseContent["result"]["postcode"].ToString();
+            Assert.That(result, Is.EqualTo("EC2Y 5AS"));
+        }
+
+        [Test]
+
+        public void ContentType_IsJson()
+        {
+            Assert.That(_singlePostcodeService.Response.ContentType, Is.EqualTo("application/json"));
+        }
+
+        [Test]
+
+        public void ConnectionIsKeepAlive()
+        {
+            var result = _singlePostcodeService.Response.Headers.Where(x => x.Name == "Connection")
+            .Select(x => x.Value).FirstOrDefault();
+            Assert.That(result, Is.EqualTo("keep-alive"));
+        }
+
+        [Test]
+
+        public void ObjectStatus_Is200()
+        {
+            Assert.That(_singlePostcodeService.ResponseObject.status, Is.EqualTo(200));
+        }
+
+        [Test]
+
+        public void StatusInResponseHeader()
+        {
+            Assert.That((int)_singlePostcodeService.Response.StatusCode, Is.EqualTo(_singlePostcodeService.ResponseObject.status));
+        }
+
+        [Test]
+
+        public void AdminDistrict_IsCityOfLondon()
+        {
+            Assert.That(_singlePostcodeService.ResponseObject.result.admin_district, Is.EqualTo("City of London"));
+        }
+    }
+}
+
